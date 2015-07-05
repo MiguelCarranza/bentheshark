654b0467a59aee4e55c3b8a4d5f16e70fc446a7c
Miguel Carranza <miguel@miguelcarranza.es>
2015-07-04 17:26:17 -0700

    Basic Shark Research Committe parser

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'pg', '~> 0.18.2'
+gem 'nokogiri', '~> 1.6.6'
+gem 'htmlentities', '~> 4.3.3'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
---
+++ app/models/src_parser.rb
@@ -0,0 +1,53 @@
+require 'open-uri'
+require 'htmlentities'
+
+class SRCParser
+  URL = ENV.fetch('SRC_URL', 
+                  'http://www.sharkresearchcommittee.com/pacific_coast_shark_news.htm')
+
+  WHITE_LIST_SPOT_NAMES = ['beach', 'harbor', 'point']
+  BLACK_LIST_SPOT_NAMES = ['memoriam', 'notice']
+
+  def self.get_sightings_info
+    sightings = []
+
+    doc = Nokogiri::HTML(open URL)
+    paragraphs = doc.css 'p'
+    candidate_paragraphs = possible_sightings paragraphs
+    sightings = candidate_paragraphs.map { |c| parse_possible_sighting(c) }
+    sightings.select { |s| !s.nil? }
+  end
+
+  private
+  def self.possible_sightings(paragraphs)
+    dash = HTMLEntities.new.decode "&#151;"
+    paragraphs.select { |p| HTMLEntities.new.decode(p.to_s).include? dash } 
+  end
+
+  def self.parse_possible_sighting(html)
+    spot = html.css('strong').first
+    spot = spot.text.strip if spot
+    spot = (validate_spot_name spot) ? spot : nil
+    date = extract_date(html.text)
+
+    make_sighting(date, spot) if date and spot
+  end
+
+  def self.validate_spot_name(spot_name)
+    (WHITE_LIST_SPOT_NAMES.select { |white| spot_name.downcase.include? white}.count > 0 ||
+     BLACK_LIST_SPOT_NAMES.select { |black| spot_name.downcase.include? black}.count == 0)
+  end
+
+  def self.extract_date(html)
+    begin 
+      Date.parse html
+    rescue ArgumentError
+    end
+  end
+
+  def self.make_sighting(date, spot)
+    { "date" => date,
+      "spot" => spot
+    }
+  end
+end
